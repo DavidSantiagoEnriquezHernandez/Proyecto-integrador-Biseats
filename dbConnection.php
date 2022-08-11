e1153f9637caf465929246cd7d8dd271357bbe1a
David Santiago Enriquez Hernandez <81264608+DavidSantiagoEnriquezHernandez@users.noreply.github.com>
2022-08-10 20:22:51 -0600

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-    $dbServer = "localhost";
-    $dbUser = "root";
-    $dbPassword = "";
-    $dbDatabase = "biseats";
+    $dbServer = "ec2-44-208-88-195.compute-1.amazonaws.com";
+    $dbUser = "rjbjhwldpbvsmo"; 
+    $dbPassword = "91e40778ef95a78bd56b55fc94704f429b9612a6da2bd0a6d6e0c604e8cb3f2d";
+    $dbDatabase = "d2ugbigss1fa5k";
 
     $connectionToDB = new mysqli($dbServer, $dbUser, $dbPassword, $dbDatabase);
         
