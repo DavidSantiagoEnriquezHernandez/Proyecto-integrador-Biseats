edc4f18298d952346c0e3fe65ee5a4fed15b2126
David Santiago Enriquez Hernandez <81264608+DavidSantiagoEnriquezHernandez@users.noreply.github.com>
2022-08-10 20:31:05 -0600

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -3,8 +3,9 @@
     $dbUser = "rjbjhwldpbvsmo"; 
     $dbPassword = "91e40778ef95a78bd56b55fc94704f429b9612a6da2bd0a6d6e0c604e8cb3f2d";
     $dbDatabase = "d2ugbigss1fa5k";
+    $port = "5432";
 
-    $connectionToDB = new mysqli($dbServer, $dbUser, $dbPassword, $dbDatabase);
+    $connectionToDB = new mysqli($dbServer, $dbUser, $dbPassword, $dbDatabase, $port);
         
     if($connectionToDB -> connect_errno){
         die("FAILURE: ".mysqli_connect_errno());
